e3e1bdbbd38de1f64ac10175517dabf7b8654b83
无处不在的草方块 <caofangkuai2022@qq.com>
2026-05-01 20:43:17 +0800

    update

--- manager/src/main/java/com/cfks/utils/TesseractHelper.java
+++ manager/src/main/java/com/cfks/utils/TesseractHelper.java
@@ -19,8 +19,8 @@ public class TesseractHelper {
     private static final String TAG = "TesseractHelper";
     private static final String TESSDATA_DIR_NAME = "tesseract";  // 私有目录下的主目录
     private static final String TESSDATA_SUB_DIR = "tessdata";    // 必须包含 tessdata 子目录
-    private static final String LANGUAGE = "equ";                 // 训练数据语言
-    private static final String TRAINEDDATA_FILENAME = "equ.traineddata";
+    private static final String LANGUAGE = "eng";                 // 训练数据语言
+    private static final String TRAINEDDATA_FILENAME = "eng.traineddata";
 
     private static volatile boolean isInitialized = false;
     private static String dataPath;  // 指向 tessdata 的父目录
