5b13f8d35406bd68f18879ba828b271dae16e97f
无处不在的草方块 <caofangkuai2022@qq.com>
2026-05-04 21:05:18 +0800

    update

---
+++ manager/src/main/java/com/cfks/badresolve/CategoriesGenerator.java
@@ -0,0 +1,11 @@
+package com.cfks.badresolve;
+
+/**
+ * 自动生成的Category常量类
+ * 用于BadResolve漏洞利用 - 创建大量categories延长Intent解析时间
+ */
+public class CategoriesGenerator {
+    public static String[] getAllCategories() {
+        return new String[] {};
+    }
+}
